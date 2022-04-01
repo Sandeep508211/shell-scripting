d50d2c488437e7199745e15ed41f1f6e3e63f34b
Sandeep Martha <sandeepvarma508211@gmail.com>
2022-04-01 20:45:41 +0530

    addded new file

--- sample.sh
+++ sample.sh
@@ -3,14 +3,3 @@
 
 
 # echo "sandeep"
-echo "devops"
-
-
-echo "hello world"
-
-echo -e "hello, \n welcome to devops"
-
-echo -e "\n hello, \n\nwelcome to devops"
-
-echo -e "\e[32mwelcome to devops"
-echo -e "\e[33msandeep"
