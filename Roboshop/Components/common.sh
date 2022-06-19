3d61c2b547e8fca3223785da192b1991696bfd51
Sandeep Martha <sandeepvarma508211@gmail.com>
2022-06-19 23:12:49 +0530

    added common file

--- Roboshop/Components/common.sh
+++ Roboshop/Components/common.sh
@@ -25,88 +25,89 @@ STAT () {
           else
             useradd roboshop &>>/tmp/roboshop.log
             STAT $?
-            fi
-            }
+          fi
+      }
 
-            SETUP_SYSTEM() {
-              HEAD "Setup SystemD Service\t\t"
-              sed -i -e 's/MONGO-DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO-ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE-ENDPOINT/catalogue.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internel/' -e 's/DBSHOST/mysql.roboshop.internal/' /home/roboshop/$1/systemd.service && mv /home/roboshop/$1/systemd.service /etc/system/system/$1.service
-              STAT $?
+      SETUP_SYSTEM() {
+        HEAD "Setup SystemD Service\t\t"
+        sed -i -e 's/MONGO-DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO-ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE-ENDPOINT/catalogue.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internel/' -e 's/DBSHOST/mysql.roboshop.internal/' /home/roboshop/$1/systemd.service && mv /home/roboshop/$1/systemd.service /etc/system/system/$1.service
+        STAT $?
 
-              HEAD "start $1 Service\t"
-              systemctl daemon-reload && systemctl enable $1 &>> /tmp/roboshop.log && systemctl restart $1 &>>/tmp/roboshop.log
-              STAT $?
-              }
+        HEAD "start $1 Service\t"
+        systemctl daemon-reload && systemctl enable $1 &>> /tmp/roboshop.log && systemctl restart $1 &>>/tmp/roboshop.log
+        STAT $?
+      }
 
-              DOWNLOAD_FROM_GITHUB() {
-                HEAD "Download App from GitHub\t"
-                curl -s -L -o /tmp/$1.zip "https://github.com/roboshop-devops-project/$1/archieve/main.zip" &>>/tmp/roboshop.log
-                STAT $?
-                HEAD "Download App from GitHub\t"
-                cd /home/roboshop && rm -rf $1 && unzip /tmp/$1.zip &>>/tmp/roboshop.log && mv $1-main $1
-                STAT $?
-                }
+      DOWNLOAD_FROM_GITHUB() {
+        HEAD "Download App from GitHub\t"
+        curl -s -L -o /tmp/$1.zip "https://github.com/roboshop-devops-project/$1/archieve/main.zip" &>>/tmp/roboshop.log
+        STAT $?
+        HEAD "Download App from GitHub\t"
+        cd /home/roboshop && rm -rf $1 && unzip /tmp/$1.zip &>>/tmp/roboshop.log && mv $1-main $1
+        STAT $?
+      }
 
-                FIX_APP_CONTENT_PERM() {
-                  HEAD "Fix Permission to App Content"
-                  chown roboshop:roboshop /home/roboshop -R
-                  STAT $?
-                  }
-                  NODEJS() {
-                    HEAD "Install Nodejs\t\t\t"
-                    yum install nodejs make gcc-c++ -y &>>/tmp/roboshop.log
-                    STAT $?
+      FIX_APP_CONTENT_PERM() {
+        HEAD "Fix Permission to App Content"
+        chown roboshop:roboshop /home/roboshop -R
+        STAT $?
+      }
+      NODEJS() {
+        HEAD "Install Nodejs\t\t\t"
+        yum install nodejs make gcc-c++ -y &>>/tmp/roboshop.log
+        STAT $?
 
-                    APP_USER_ADD
-                    DOWNLOAD_FROM_GITHUB $1
 
-                    HEAD "Install NodeJS Dependencies\t"
-                    cd /home/roboshop/$1 && npm install --unsafe-perm &>>/tmp/roboshop.log
-                    STAT $?
+        APP_USER_ADD
+        DOWNLOAD_FROM_GITHUB $1
 
-                    FIX_APP_CONTENT_PERM
+        HEAD "Install NodeJS Dependencies\t"
+        cd /home/roboshop/$1 && npm install --unsafe-perm &>>/tmp/roboshop.log
+        STAT $?
 
-                    SETUP_SystemD"$1"
-                    }
+        FIX_APP_CONTENT_PERM
 
-                    MAVEN() {
-                      HEAD "Install Maven"
-                      yum install maven -y &>> /tmp/roboshop.log
-                      STAT $?
+        SETUP_SystemD"$1"
+      }
 
-                      APP_USER_ADD
-                      DOWNLOAD_FROM_GITHUB $1
+      MAVEN() {
+        HEAD "Install Maven"
+        yum install maven -y &>> /tmp/roboshop.log
+        STAT $?
 
-                      HEAD "Make Application Package"
-                      cd /home/roboshop/$1 && mvn clean package &>> /tmp/roboshop.log && mv target/$1-1.0.jar &>>/tmp/roboshop.log
-                      STAT $?
+        APP_USER_ADD
+        DOWNLOAD_FROM_GITHUB $1
 
-                      FIX FIX_APP_CONTENT_PERM
+        HEAD "Make Application Package"
+        cd /home/roboshop/$1 && mvn clean package &>> /tmp/roboshop.log && mv target/$1-1.0.jar &>>/tmp/roboshop.log
+        STAT $?
 
-                      SETUP-SYSTEMD "$1"
-                      }
+        FIX FIX_APP_CONTENT_PERM
 
-                      PYTHON3() {
-                        HEAD "Install python3"
-                        yum install python36 gcc python3-devel -y&>>/tmp/roboshop.log
-                        STAT $?
+        SETUP-SYSTEMD "$1"
+      }
 
-                        APP_USER_ADD
-                        DOWNLOAD_FROM_GITHUB $1
+      PYTHON3() {
+        HEAD "Install python3"
+        yum install python36 gcc python3-devel -y&>>/tmp/roboshop.log
+        STAT $?
 
-                        HEAD "Install Python Deps
-                        cd /home/roboshop/$1 && pip3 -r requirement.txt &>>/tmp/roboshop.log
-                        STAT $?
+        APP_USER_ADD
+        DOWNLOAD_FROM_GITHUB $1
 
-                        USER_ID=$(id -u roboshop)
-                        GROUP_ID=$(id -g roboshop)
+        HEAD "Install Python Deps
+        cd /home/roboshop/$1 && pip3 -r requirement.txt &>>/tmp/roboshop.log
+        STAT $?
 
-                        HEAD "Update App Configuration"
-                        sed -i -e "/uid/ c uid=$(USER_ID)" -e "/gid/ c gid=$(GROUP_ID)" /home/roboshop/$1/$1.ini
-                        STAT $?
+        USER_ID=$(id -u roboshop)
+        GROUP_ID=$(id -g roboshop)
 
-                        SETUP_SYSTEMD "$1"
-                        }
+        HEAD "Update App Configuration"
+        sed -i -e "/uid/ c uid=$(USER_ID)" -e "/gid/ c gid=$(GROUP_ID)" /home/roboshop/$1/$1.ini
+        STAT $?
+
+        SETUP_SYSTEMD "$1"
+        }
 
 
 
